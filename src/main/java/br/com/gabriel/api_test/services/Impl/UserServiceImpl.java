6f65a0ce849209e5307088945833df336cfd4ad7
gsouza590 <gsouza590@gmail.com>
2024-07-02 14:59:52 -0300

    Add exception ObjectNotFound

--- src/main/java/br/com/gabriel/api_test/services/Impl/UserServiceImpl.java
+++ src/main/java/br/com/gabriel/api_test/services/Impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import br.com.gabriel.api_test.models.User;
 import br.com.gabriel.api_test.repositories.UserRepository;
 import br.com.gabriel.api_test.services.UserService;
+import br.com.gabriel.api_test.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -15,7 +16,7 @@ public class UserServiceImpl implements UserService{
 	
 	@Override
 	public User findById(Integer id) {
-		return repository.findById(id).orElse(null);
+		return repository.findById(id).orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
