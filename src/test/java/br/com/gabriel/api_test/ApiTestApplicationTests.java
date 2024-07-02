c0815ce013e9105228691d0b2c7409111f62cfe5
gsouza590 <gsouza590@gmail.com>
2024-07-02 12:58:17 -0300

    create project

---
+++ src/test/java/br/com/gabriel/api_test/ApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.api_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
