8d6175286f76ba8b3f53954fed1a2a15c27562a4
gsouza590 <gsouza590@gmail.com>
2024-07-05 15:35:20 -0300

    create tests findAll, create, update and delete

--- src/test/java/br/com/gabriel/api_test/controllers/UserControllerTest.java
+++ src/test/java/br/com/gabriel/api_test/controllers/UserControllerTest.java
@@ -4,7 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,6 +19,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import br.com.gabriel.api_test.models.User;
@@ -47,7 +54,7 @@ public class UserControllerTest {
 		dto = new UserDto(ID, NAME, EMAIL, PASSWORD);
 
 	}
-	
+
 	@Test
 	void whenFindByIdThenReturnSuccess() {
 		when(service.findById(anyInt())).thenReturn(user);
@@ -64,5 +71,66 @@ public class UserControllerTest {
 	      assertEquals(NAME, response.getBody().getName());
 	      assertEquals(EMAIL, response.getBody().getEmail());
 	      assertEquals(PASSWORD, response.getBody().getPassword());
+	}
+
+	@Test
+	void whenFindAllThenReturnAListOfUserDto() {
+		when(service.findAll()).thenReturn(List.of(user));
+		when(mapper.map(any(), any())).thenReturn(dto);
+		
+		ResponseEntity<List<UserDto>> response = controller.findAll();
+		
+		assertNotNull(response);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(ArrayList.class,response.getBody().getClass());
+		assertEquals(UserDto.class,response.getBody().get(0).getClass());
+		
+		
+		  assertEquals(ID, response.getBody().get(0).getId());
+	      assertEquals(NAME, response.getBody().get(0).getName());
+	      assertEquals(EMAIL, response.getBody().get(0).getEmail());
+	      assertEquals(PASSWORD, response.getBody().get(0).getPassword());
+	}
+
+	@Test
+	void whenCreateThenReturnCreated() {
+		when(service.create(any())).thenReturn(user);
+		ResponseEntity<UserDto> response= controller.create(dto);
+		
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(HttpStatus.CREATED, response.getStatusCode());
+		assertNotNull(response.getHeaders().get("Location"));
+		
+	}
+
+	@Test
+	void whenUpdateThenReturnSucess() {
+		when(service.update(dto)).thenReturn(user);
+		when(mapper.map(any(), any())).thenReturn(dto);
+		ResponseEntity<UserDto> response = controller.update(ID,dto);
+		
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(UserDto.class, response.getBody().getClass());
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		
+
+		assertEquals(ID, response.getBody().getId());
+	    assertEquals(NAME, response.getBody().getName());
+	    assertEquals(EMAIL, response.getBody().getEmail());
+	    
+	}
+	
+	@Test
+	void whenDeleteThenReturnSuccess() {
+		doNothing().when(service).delete(anyInt());
+		ResponseEntity<UserDto> response = controller.delete(ID);
+		
+		assertNotNull(response);
+		assertEquals(ResponseEntity.class, response.getClass());
+		verify(service,times(1)).delete(anyInt());
+		assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
 	}
 }
